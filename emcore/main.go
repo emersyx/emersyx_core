5dc941399f39853140f926e4afe566b512b3a14d
Alexandru Geana <alex@alegen.net>
2018-01-14 00:19:09 +0100

    update logging code in main.go

--- emcore/main.go
+++ emcore/main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"emersyx.net/emersyx_log/emlog"
-	golog "log"
-)
-
-var log *golog.Logger
-
 func main() {
-	log := emlog.NewEmlogStdout("emcore")
-	log.Print("emersyx core")
+	// parse the command line flags
+	parseFlags()
+
+	el.Println(1, "starting emersyx")
 }
